49f17d492de641f481c7a73a8e053806f46a6268
Sibuta <kelvin.sibuta@aws.ajiraemobilis.orgclear>
2023-12-29 12:23:35 +0300

    first commit

---
+++ sim.js/sibuta.js
@@ -0,0 +1,21 @@
+//a variable is a container for storing data
+//Two steps
+//1. Declaring the variable (var, let, const)
+//2. Assigning the variable(= assignment operater)
+let message="Today is Thirsty Thursday";//This is a string
+var greetings="Good day to code";//This is a string
+console.log(message);
+console.log(greetings);
+let age=21;//this is a number
+var year=2011;//This is a number
+let firstName = "Kelvin";
+let lastName = "Sibuta";
+let student = false//This is a boolean value
+console.log(message);
+console.log(greetings);
+console.log(age);
+console.log(year);
+console.log(firstName);
+console.log(lastName);
+console.log(student);
+console.log("Hello, "+ firstName +""+ lastName,);
